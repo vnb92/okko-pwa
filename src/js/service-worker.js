a63af555528423c4a1a9639258ab765ceda8b0ae
Богатырёв Владимир Николаевич <VNBogatyrev@sberbank.ru>
2020-08-09 07:20:50 +0300

    service-wotker scope

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,28 +1,28 @@
 const CACHE = 'cache-only';
 
 self.addEventListener('install', (event) => {
-    // console.log('SW установлен');
+    console.log('SW установлен');
 
     event.waitUntil(
         caches.open(CACHE).then((cache) => cache.addAll([
-            '/',
-            '/bundle.js',
-            '/styles.css',
+            '/okko-pwa/',
+            '/okko-pwa/bundle.js',
+            '/okko-pwa/styles.css',
         ]))
     );
 });
 
 self.addEventListener('activate', (event) => {
-    // console.log('SW активирован');
+    console.log('SW активирован');
 });
 
 self.addEventListener('fetch', (event) => {
-    // console.log('SW: происходит запрос на сервер ');
+    console.log('SW: происходит запрос на сервер');
     event.respondWith(fromCache(event.request))
 });
 
 function fromCache(request) {
-    // console.log('SW from cache');
+    console.log('SW from cache', request);
     
     return caches.open(CACHE).then(
         (cache) => cache.match(request).then(
